09393e0576caa8f5e063e19d0353bfbd763ea580
yvi7693 <yvi_7693@icloud.com>
2026-02-03 10:39:36 +0300

    Update Practice_3_1.py

--- Practice_3_1.py
+++ Practice_3_1.py
@@ -1,3 +1,5 @@
+# task2
+
 class Smartphone:
     brand: str
     model: str
@@ -70,11 +72,136 @@ class Smartphone:
 
         self.charge -= decrease_charge
 
+
     def get_is_work(self) -> bool:
         return self.is_work
 
     def get_charge(self) -> int:
         return self.charge
+
+    def get_ram(self) -> int:
+        return self.ram
+
+    def get_free_storage(self) -> float:
+        return self.free_storage
+
+    def get_storage(self) -> int:
+        return self.storage
+
+    def get_operating_system(self) -> str:
+        return self.operating_system
+
+    def get_model(self) -> str:
+        return self.model
+
+    def get_brand(self) -> str:
+        return self.brand
+
+
+    def set_operating_system(self, new_system: str) -> None:
+        self.operating_system = new_system
+
+    def set_storage(self, new_storage: int) -> None:
+        self.storage = new_storage
+
+    def set_ram(self, new_ram: int) -> None:
+        self.ram = new_ram
+
+    def set_charge(self, new_charge: int) -> None:
+        self.charge = new_charge
+
+    def set_is_work(self, new_is_work: bool) -> None:
+        self.is_work = new_is_work
+
+
+
+# task3
+
+class Ingredient:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def __str__(self):
+        return self.name
+
+
+class Potion:
+
+    name: str
+    ingredients: list[Ingredient]
+    level_difficult: int
+    effect: str
+    is_readiness: bool
+
+
+    def __init__(self, name: str, ingredients: list[Ingredient], level_difficult: int, effect: str, is_readiness: bool):
+        self.name = name
+        self.ingredients = ingredients
+        self.level_difficult = level_difficult
+        self.effect = effect
+        self.is_readiness = is_readiness
+
+    def __str__(self):
+        return f"Name: {self.name} \ningredients: {self.get_ingredients()} \nlevel difficult: {self.level_difficult} \neffect: {self.effect} \nis readiness: {self.is_readiness}"
+
+    def add_ingredient(self, new_ingredient: Ingredient) -> None:
+        self.ingredients.append(new_ingredient)
+
+    def delete_ingredient(self, delete_ingredient: Ingredient):
+        global delete_index
+
+        for i in range(len(self.ingredients)):
+            if self.ingredients[i] == delete_ingredient:
+                delete_index = i
+
+        self.ingredients.pop(delete_index)
+
+    def make_potion(self) -> str:
+        return "Зелье приготовлено"
+
+
+    def set_name(self, new_name: str) -> None:
+        self.name = new_name
+
+    def set_level_difficult(self, new_level_difficult: int) -> None:
+        self.level_difficult = new_level_difficult
+
+    def set_effect(self, new_effect) -> None:
+        self.effect = new_effect
+
+    def set_is_readiness(self, new_is_readiness):
+        self.is_readiness = new_is_readiness
+
+
+    def get_name(self) -> str:
+        return self.name
+
+    def get_ingredients(self) -> list[str]:
+
+        return [ingredient.name for ingredient in self.ingredients]
+
+    def get_level_difficult(self) -> int:
+        return self.level_difficult
+
+    def get_effect(self) -> str:
+        return self.effect
+
+    def get_is_readiness(self) -> bool:
+        return self.is_readiness
+
+
+bubotuber_pus = Ingredient("Слизь бубонтюбера")
+boomslang_skin = Ingredient("Кожа бумсланга")
+
+polyjuice = Potion("Оборотное зелье", [bubotuber_pus, boomslang_skin], 9, "Активность", False)
+
+print(polyjuice)
+
+
+
+
 
 
 
