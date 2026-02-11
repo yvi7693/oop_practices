1842d31efc6087b8fda67442112c2120bce8157c
yvi7693 <yvi_7693@icloud.com>
2026-02-11 08:28:02 +0300

    Update hw_3_2.py

--- hw3_2/hw_3_2.py
+++ hw3_2/hw_3_2.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+import math
 
 # task 3
 
@@ -10,12 +11,24 @@ class Coord:
     y: int
 
     def __init__(self, x: int, y: int):
-        self.x = x
-        self.y = y
+        self.__x = x
+        self.__y = y
 
     def __str__(self):
-        return (f"x: {self.x}"
-                f"y: {self.y}")
+        return (f"({self.__x},{self.__y})")
+
+    def get_x(self) -> int:
+        return self.__x
+
+    def set_x(self, new_x: int) -> None:
+        self.__x = new_x
+
+    def get_y(self) -> int:
+        return self.__y
+
+    def set_y(self, new_y: int) -> None:
+        self.__y = new_y
+
 
 
 class ModelWindow:
@@ -28,79 +41,85 @@ class ModelWindow:
     has_frame: bool
 
     def __init__(self, heading: str, upper_left_corner: Coord, horizontal_size: int, vertical_size: int, color: str, is_visible: bool, has_frame:  bool):
-        self.heading = heading
-        self.upper_left_corner = upper_left_corner
-        self.vertical_size = vertical_size
-        self.horizontal_size = horizontal_size
-        self.color = color
-        self.is_visible = is_visible
-        self.has_frame = has_frame
+        self.__heading = heading
+        self.__upper_left_corner = upper_left_corner
+        self.__vertical_size = vertical_size
+        self.__horizontal_size = horizontal_size
+        self.__color = color
+        self.__is_visible = is_visible
+        self.__has_frame = has_frame
 
     def __str__(self):
-        return (f"heading: {self.heading}"
-                f"upper left corner: {self.upper_left_corner}"
-                f"horizontal: {self.horizontal_size}"
-                f"vertical: {self.vertical_size}"
-                f"color: {self.color}"
-                f"is visible: {self.is_visible}"
-                f"has frame: {self.has_frame}")
+        return (f"heading: {self.__heading}\n"
+                f"upper left corner: {self.__upper_left_corner}\n"
+                f"horizontal: {self.__horizontal_size}\n"
+                f"vertical: {self.__vertical_size}\n"
+                f"color: {self.__color}\n"
+                f"is visible: {self.__is_visible}\n"
+                f"has : {self.__has_frame}\n")
 
     def move_x(self, shift: int) -> None:
-        new_x = self.upper_left_corner.x + shift
+        new_x = self.__upper_left_corner.get_x() + shift
 
-        if new_x > (self.upper_left_corner.x + self.horizontal_size):
+        if new_x > (self.__upper_left_corner.get_x() + self.__horizontal_size):
             raise ValueError
 
-        self.upper_left_corner.x += shift
+        self.__upper_left_corner.set_x(shift + self.__upper_left_corner.get_x())
 
     def move_y(self, shift: int) -> None:
-        new_y = self.upper_left_corner.y + shift
+        new_x = self.__upper_left_corner.get_y() + shift
 
-        if new_y > (self.upper_left_corner.y + self.vertical_size):
+        if new_x > (self.__upper_left_corner.get_y() + self.__horizontal_size):
             raise ValueError
 
-        self.upper_left_corner.y += shift
+        self.__upper_left_corner.set_y(shift + self.__upper_left_corner.get_y())
 
 
     def set_vertical(self, new_vertical_size: int) -> None:
-        if (new_vertical_size + self.upper_left_corner.y) > BORDER_Y:
-            raise ValueError
+            if (new_vertical_size + self.get_upper_left_corner().y) > BORDER_Y:
+                raise ValueError
 
-        self.vertical_size = new_vertical_size
+            self.__vertical_size = new_vertical_size
 
     def set_horizontal(self, new_horizontal_size) -> None:
-        if (new_horizontal_size + self.upper_left_corner.y) > BORDER_X:
+        if (new_horizontal_size + self.__upper_left_corner.get_y()) > BORDER_X:
             raise ValueError
 
-        self.horizontal_size = new_horizontal_size
+        self.__horizontal_size = new_horizontal_size
 
     def set_color(self, new_color) -> None:
-        self.color = new_color
+        self.__color = new_color
 
     def set_is_visible(self, new_condition: bool) -> None:
-        self.is_visible = new_condition
+        self.__is_visible = new_condition
 
     def set_has_frame(self, new_condition: bool) -> None:
-        self.has_frame = new_condition
+        self.__has_frame = new_condition
 
 
     def get_heading(self) -> str:
-        return self.heading
+        return self.__heading
 
     def get_upper_left_corner(self) -> Coord:
-        return self.upper_left_corner
+        return self.__upper_left_corner
 
     def get_horizon_size(self) -> int:
-        return self.horizontal_size
+        return self.__horizontal_size
 
     def get_vertical_size(self) -> int:
-        return self.vertical_size
+        return self.__vertical_size
 
     def get_is_visible(self) -> bool:
-        return self.is_visible
+        return self.__is_visible
 
     def get_has_frame(self) -> bool:
-        return self.has_frame
+        return self.__has_frame
+
+game = ModelWindow("Игра", Coord(3,6), 800, 500, "red", True, True)
+
+game.move_x(200)
+game.move_y(200)
+print(game)
 
 
 # task 5
@@ -123,25 +142,39 @@ class Vector:
         return Vector(x,y,z)
 
     def __sub__(self, right: Vector):
-        x = self.x + right.x
-        y = self.y + right.y
-        z = self.z + right.z
+        x = self.x - right.x
+        y = self.y - right.y
+        z = self.z - right.z
 
         return Vector(x,y,z)
 
-    def __mul__(self, right: Vector):
-        x = self.x * right.x
-        y = self.y * right.y
-        z = self.z * right.z
+    def __mul__(self, right: Vector | float):
+        if type(right) == Vector:
+            x = (self.y * right.z) - (self.z * right.y)
+            y = (self.z * right.x) - (self.x * right.z)
+            z = (self.x * right.y) - (self.y * right.x)
+
+            return Vector(x,y,z)
+
+        x = self.x * right
+        y = self.y * right
+        z = self.z * right
 
         return Vector(x,y,z)
 
+    def __str__(self):
+        return f"({self.x},{self.y},{self.z})"
+
+    def length(self) -> float:
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
 
 
+vector1 = Vector(3,1,6)
+vector2 = Vector(4,3,8)
 
-
-
-
-
-
-
+print(vector1 + vector2)
+print(vector1 - vector2)
+print(vector1 * vector2)
+print(vector1 * 5)
+print(vector1.length())
+print(f"{vector1} {vector2}")
